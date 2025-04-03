161626969e886ea2358c1caf32ecef2ba986c054
keerthana9944 <herald.keerthana@gmail.com>
2025-04-03 12:03:33 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-const express = require('express');
-const { resolve } = require('path');
+const express= require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const menuItem = require("./model/menuSchema");
 
 const app = express();
-const port = 3010;
+app.use(express.json());
 
-app.use(express.static('static'));
+const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.get("/", (req,res) => {
+    res.send("Server is running successfully");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch((err)=> console.error("Connection failed", err));
+
+
+//Updating existing menu items
+app.put("/menu/:id", async(req, res) => {
+    try{
+        const {name, description, price} = req.body;
+        const updateitems = await menuItem.findByIdAndUpdate(req.params.id, {name,description,price}, {new:true});
+
+        if(!updateitems){
+            res.status(404).json({message: "Items not found"});
+        }
+        res.json(updateitems);
+    }
+
+    catch(error){
+        res.status(500).json({message: error.message});
+    }
+});
+
+//Deleting a menu item
+app.delete("/menu/:id", async(req,res) => {
+    try{
+        const deleteItem = await menuItem.findByIdAndDelete(req.params.id);
+
+        if(!deleteItem){
+            res.status(404).json({message: "Items not found"});
+        };
+
+        res.status(200).json({message:"deleted successfully"});
+    }
+    catch(error){
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
